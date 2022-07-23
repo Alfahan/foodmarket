30a9bc97d27510a7d040bb36d1751cda5154ff5a
Alfahan <ali.farhan160@gmail.com>
2022-07-23 11:23:05 +0700

    API logout

--- foodmarket-backend/app/Http/Controllers/API/UsersController.php
+++ foodmarket-backend/app/Http/Controllers/API/UsersController.php
@@ -98,4 +98,11 @@ class UsersController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
 }
